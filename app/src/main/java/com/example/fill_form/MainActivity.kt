e79b90114b3ff25819cd35e6927f60c1af24b8ad
Pankaj <pankaj.pal20301@gmail.com>
2023-03-04 13:08:20 +0530

    error

--- app/src/main/java/com/example/fill_form/MainActivity.kt
+++ app/src/main/java/com/example/fill_form/MainActivity.kt
@@ -1,44 +1,76 @@
 package com.example.fill_form
 
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.*
+import androidx.core.graphics.green
+import androidx.core.view.get
+import androidx.core.view.isEmpty
+import androidx.databinding.DataBindingUtil
+import com.example.fill_form.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bind: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val frist_name :EditText =findViewById(R.id.f_name)
-        val last_name :EditText =findViewById(R.id.l_name)
-        val phn :   EditText= findViewById(R.id.pn_no)
-        val Al_phn : EditText = findViewById(R.id.Al_phn)
-        val E_mail:EditText= findViewById(R.id.e_mail)
-        val dob:EditText= findViewById(R.id.dob)
-        val btn : Button= findViewById(R.id.btn_id)
+        bind = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val p_name : TextView = findViewById(R.id.full_name)
-        val p_phn : TextView = findViewById(R.id.ur_phn)
-        val p_alphn : TextView = findViewById(R.id.ur_alphn)
-        val p_email : TextView = findViewById(R.id.ur_email)
-        val p_dob : TextView = findViewById(R.id.ur_dob)
+        bind.btnId.setOnClickListener {
 
-        btn.setOnClickListener {
 
-            var f_name = frist_name.text.toString()
-            var l_name = last_name.text.toString()
-            var ph_no = phn.text.toString()
-            var Alph_no = Al_phn.text.toString()
-            var Email = E_mail.text.toString()
-            var DOB = dob.text.toString()
+            if (bind.fnameId.editText!!.text.toString().isNotEmpty())
+            {
+                if (bind.lnameId.editText!!.text.toString().isNotEmpty())
+                {
+                    if (bind.pnNo.editText!!.text.toString().isNotEmpty())
+                    {
+                        if (bind.AlPhn.editText!!.text.toString().isNotEmpty())
+                        {
+                            if (bind.eMail.editText!!.text.toString().isNotEmpty())
+                            {
+                                if (bind.dob.editText!!.text.toString().isNotEmpty())
+                                {
+                                    bind.fullName.setText("Your name : "+ bind.fnameId.editText!!.text.toString() +" "+ bind.lnameId.editText!!.text.toString())
+                                    bind.urPhn.setText("phone number : +91"+bind.pnNo.editText!!.text.toString())
+                                    bind.urAlphn.setText("Alternative ph.no. : +91"+bind.AlPhn.editText!!.text.toString())
+                                    bind.urEmail.setText("E-mail Address: "+bind.eMail.editText!!.text.toString() +"gmail.com")
+                                    bind.urDob.setText("D.O.B :"+bind.dob.editText!!.text.toString())
 
-            p_name.setText("Your name :$f_name$l_name")
-            p_phn.setText("phone number :$ph_no")
-            p_alphn.setText("Alternative ph.no. :$Alph_no")
-            p_email.setText("E-mail Address:$Email")
-            p_dob.setText("D.O.B :$DOB")
+                                } else {
+                                    Toast.makeText(this, "Field cant be empty", Toast.LENGTH_SHORT)
+                                        .show()
+                                    bind.dob.requestFocus()
+                                }
+                            } else {
+                                Toast.makeText(this, "Field cant be empty", Toast.LENGTH_SHORT)
+                                    .show()
+                                bind.eMail.requestFocus()
+                            }
+                        } else {
+                            Toast.makeText(this, "Field cant be empty", Toast.LENGTH_SHORT).show()
+                            bind.AlPhn.requestFocus()
+                        }
+                    } else {
+                        Toast.makeText(this, "Field cant be empty", Toast.LENGTH_SHORT).show()
+                        bind.pnNo.requestFocus()
+                    }
+                }
+                else {
+                    Toast.makeText(this, "Field cant be empty", Toast.LENGTH_SHORT).show()
+                    bind.lnameId.requestFocus()
+                }
+            }
+            else {
+                Toast.makeText(this, "Field cant be empty", Toast.LENGTH_SHORT).show()
+                bind.fnameId.requestFocus()
+            }
+
+
+
         }
+
     }
 }
